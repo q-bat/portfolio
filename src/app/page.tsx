e55a9995ce3593276f59847b2c2ec66c91e0983e
q-bat <quinn.l.battle@gmail.com>
2025-08-21 17:17:35 -0500

    Adjusted main divs to fit my desired layout. Still working on it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 export default function Home() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-  <div className="font-sans grid grid-rows-[auto_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-8">
+  <div className=" font-sans grid grid-rows-[auto_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-8">
       {/* Navbar Header */}
       <header className="row-start-1 w-full flex justify-between items-center py-2 px-4 bg-white dark:bg-black shadow-md relative">
         {/* Logo & Title */}
@@ -47,102 +47,39 @@ export default function Home() {
           </nav>
         )}
       </header>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <img
-        className="rounded-full"
-        src="./QB-Professional.jpg"
-        alt="Quinn Professional pic"
-        width={300}
-        height={300}
-        loading="eager"
-        />
-        <ol className="font-mono text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            {/*Save and see your changes instantly. */}
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="dark:invert"
-              src="./vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className=" fixed top-20 grid grid-cols-1 sm:grid-cols-2 gap-8 row-start-2  sm:items-start w-full max-w-5xl px-4 mx-auto">
+        <div className=" flex flex-col items-center sm:items-start gap-8">
+          <img
+            className="rounded-full"
+            src="./QB-Professional.jpg"
+            alt="Quinn Professional pic"
+            width={300}
+            height={300}
+            loading="eager"
+          />
+        </div>
+        <div className="flex flex-col gap-4 items-center sm:items-start">
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <a>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+               et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum. At vero eos et accusamus et iusto odio
+                   dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et
+                    quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
+                     officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis
+                      est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque
+                       nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est,
+                        omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum
+                         necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+                          Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores
+                           alias consequatur aut perferendis doloribus asperiores repellat.
+            </a>
+          </div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        {/*}
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-        */}
       </footer>
     </div>
   );
