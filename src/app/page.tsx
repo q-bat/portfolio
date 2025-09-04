51988fd2aa7e2776ea2f6b336afeeb5bafc84c85
q-bat <quinn.l.battle@gmail.com>
2025-09-04 18:34:39 -0500

    Finished about section!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 export default function Home() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-  <div className="font-sans grid grid-rows-[auto_1fr_20px] min-h-screen pb-20 gap-8">
+    <div className="font-sans grid grid-rows-[auto_1fr_20px] min-h-screen pb-20 gap-6">
       {/* Navbar Header */}
       <header className="sticky top-0 row-start-1 w-full flex justify-between items-center py-2 px-4 bg-white dark:bg-black shadow-md relative">
         {/* Logo & Title */}
@@ -47,36 +47,34 @@ export default function Home() {
           </nav>
         )}
       </header>
-      <main className="flex top-20 grid sm:grid-cols-2 gap-8 px-8 row-start-2 justify-start items-start w-full max-w-6xl mx-auto">
-        <div className="bg-gray-400 rounded-xl p-4 flex flex-row items-start gap-4">
-          <img
-            className="rounded-full h-40 w-40 sm:h-80 sm:w-80"
-            src="./QB-Professional.jpg"
-            alt="Quinn Professional pic"
-            loading="eager"
-          />
-          <div className="text-lg">
-            <a>Software Developer</a>
+      <main className="flex top-20 grid gap-4 px-6 row-start-2 justify-start items-start w-full max-w-6xl mx-auto">
+        <div>
+          <div className="bg-gray-400 rounded-xl p-3 float-left mr-6 mb-4 min-w-md max-h-96 flex items-center gap-2">
+            <img
+              className="rounded-full h-40 w-40 sm:h-80 sm:w-80"
+              src="./QB-Professional.jpg"
+              alt="Quinn Professional pic"
+              loading="eager"
+            />
+            <div className="flex flex-col justify-center items-center w-full h-full p-3">
+              <span className="text-lg font-semibold">Software Developer</span>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-4 items-center sm:items-start">
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-               et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum. At vero eos et accusamus et iusto odio
-                   dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et
-                    quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
-                     officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis
-                      est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque
-                       nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est,
-                        omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum
-                         necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
-                          Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores
-                           alias consequatur aut perferendis doloribus asperiores repellat.
-            </a>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. At vero eos et accusamus et iusto odio
+            dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et
+            quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
+            officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis
+            est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque
+            nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est,
+            omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum
+            necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+            Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores
+            alias consequatur aut perferendis doloribus asperiores repellat.
           </div>
         </div>
       </main>
